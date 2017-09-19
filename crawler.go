4f6903d54fb1556a4c47c761b5972466e8c4e6df
Lepikov Stanislav <s.lepikov@semrush.com>
2017-09-19 12:14:12 +0300

    2

--- crawler.go
+++ crawler.go
@@ -16,8 +16,8 @@ import (
 func getSitesList() []string {
 	sitesList := []string{
 		"http://donothingfor2minutes.com/",
-		// "http://stenadobra.ru/",
-		// "http://humandescent.com",
+		"http://stenadobra.ru/",
+		"http://humandescent.com",
 		// "http://thefirstworldwidewebsitewerenothinghappens.com",
 		// "http://button.dekel.ru",
 		// "http://www.randominio.com/",
@@ -51,46 +51,67 @@ type BacklinksList struct {
 }
 
 func crawler(urls chan string, body chan io.Reader, errors chan error, wg *sync.WaitGroup) {
-	defer wg.Done()
-	url := <- urls
-	fmt.Println("start crawl", url)
+	for {
+		defer fmt.Println("ae1")
+		url := <- urls
+		fmt.Println("start crawl", url)
 
-	resp, err := http.Get(url)
-	if (err != nil) {
-		errors <- err
-	}
+		resp, err := http.Get(url)
+		if (err != nil) {
+			errors <- err
+		}
 
-	if resp.StatusCode == 200 {
-	    body <- resp.Body
+		if resp.StatusCode == 200 {
+		    body <- resp.Body
+		}
+		wg.Done()
 	}
 }
 
 func parser(body chan io.Reader, backlinks chan BacklinksList, errors chan error, wg *sync.WaitGroup) {
-	defer wg.Done()
-	fmt.Println("start parse")
+	for {
+		// fmt.Println("start parse")
 
-	doc, err := goquery.NewDocumentFromReader(<- body)
- 	if err != nil {
-		fmt.Println(err)
- 		errors <- err
+		doc, err := goquery.NewDocumentFromReader(<- body)
+	 	if err != nil {
+			fmt.Println(err)
+	 		errors <- err
+		}
+
+		doc.Find("a").Each(func(i int, s *goquery.Selection) {
+			Title := strings.TrimSpace(s.Text())
+			Href, _ := s.Attr("href")
+
+			fmt.Println("   ", Title, ":", Href)
+		})
+		wg.Done()
 	}
-
-	doc.Find("a").Each(func(i int, s *goquery.Selection) {
-		Title := strings.TrimSpace(s.Text())
-		Href, _ := s.Attr("href")
-
-		fmt.Println("   ", Title, ":", Href)
-	})
 }
 
-func getFromQueue(urls chan string, wg *sync.WaitGroup) {
-	defer wg.Done()
+func getFromQueue(index int, urls chan string, wg *sync.WaitGroup) {
+	// defer wg.Done()
 	sitesList := getSitesList()
-	fmt.Println("getFromQueue")
+	// fmt.Println("getFromQueue")
 
 	for _, url := range sitesList {
 		urls <- url
+		// RemoveFromQueue(sitesList, index)
+		wg.Done()
 	}
+
+	// urls <- sitesList[index]
+	// RemoveFromQueue(sitesList, index)
+}
+
+// func rangeChan(urls chan string, wg *sync.WaitGroup) {
+// 	defer wg.Done()
+// 	fmt.Println(<- urls)
+// 	go getFromQueue(urls, wg)
+// 	go rangeChan(urls, wg)
+// }
+
+func RemoveFromQueue(s []string, index int) []string {
+    return append(s[:index], s[index+1:]...)
 }
 
 func main() {
@@ -103,9 +124,11 @@ func main() {
 	body := make(chan io.Reader)
 	backlinks := make(chan BacklinksList)
 	errors := make(chan error)
+	sitesList := getSitesList()
+	sitesListLength := len(sitesList)
 
-	wg.Add(3)
-	go getFromQueue(urls, &wg)
+	wg.Add(sitesListLength*3)
+	go getFromQueue(0, urls, &wg)
 	go crawler(urls, body, errors, &wg)
 	go parser(body, backlinks, errors, &wg)
 	wg.Wait()
@@ -116,3 +139,38 @@ func main() {
 	fmt.Println("\n")
 	fmt.Println(end.Sub(start))
 }
+
+// package main
+//
+// import (
+//     "fmt"
+//     "time"
+// )
+//
+// func pinger(c chan string) {
+//     for i := 0; ; i++ {
+//         c <- "ping"
+//     }
+// }
+// func ponger(c chan string) {
+//     for i := 0; ; i++ {
+//         c <- "pong"
+//     }
+// }
+// func printer(c chan string) {
+//     // for {
+//     msg := <- c
+//     fmt.Println(msg)
+//     time.Sleep(time.Second * 1)
+//     // }
+// }
+// func main() {
+//     var c chan string = make(chan string)
+//
+//     go pinger(c)
+//     go ponger(c)
+//     go printer(c)
+//
+//     var input string
+//     fmt.Scanln(&input)
+// }
